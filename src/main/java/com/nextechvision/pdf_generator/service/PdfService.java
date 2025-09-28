eb7d29d91f649b136740737062c1eb86e48301fa
MustkeemBaraskar <itservices@instagrp.com>
2025-09-28 14:45:00 +0530

    Added footer and page number throgh pdf box

--- src/main/java/com/nextechvision/pdf_generator/service/PdfService.java
+++ src/main/java/com/nextechvision/pdf_generator/service/PdfService.java
@@ -1,10 +1,16 @@
 package com.nextechvision.pdf_generator.service;
 
 import com.openhtmltopdf.pdfboxout.PdfRendererBuilder;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
+import org.apache.pdfbox.pdmodel.common.PDRectangle;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
+import java.io.ByteArrayOutputStream;
 import java.io.FileOutputStream;
 import java.io.OutputStream;
 import java.nio.file.FileSystems;
@@ -13,7 +19,6 @@ import java.util.Map;
 @Service
 public class PdfService {
 
-    /* Thymeleaf template engine for processing HTML templates */
     private final TemplateEngine templateEngine;
 
     public PdfService(TemplateEngine templateEngine) {
@@ -21,32 +26,53 @@ public class PdfService {
     }
 
     /**
-     * Generates a PDF from Thymeleaf template using provided data.
+     * Generates a PDF from Thymeleaf template and adds page numbers.
      */
     public void generatePdf(Map<String, Object> data) throws Exception {
-        /* Create Thymeleaf context and set template variables */
         Context context = new Context();
         context.setVariables(data);
 
-        /* Process the HTML template */
+        // Process Thymeleaf HTML template
         String html = templateEngine.process("statement", context);
 
-        try (OutputStream os = new FileOutputStream("statement.pdf")) {
-            /* Initialize PDF renderer */
-            PdfRendererBuilder builder = new PdfRendererBuilder();
-            builder.useFastMode();
+        // Step 1: Render HTML to PDF in-memory
+        ByteArrayOutputStream pdfOutputStream = new ByteArrayOutputStream();
+        PdfRendererBuilder builder = new PdfRendererBuilder();
+        builder.useFastMode();
 
-            /* Set base URI for resolving images and other resources */
-            String baseUri = FileSystems.getDefault()
-                    .getPath("src", "main", "resources", "static")
-                    .toUri()
-                    .toString();
+        // Base URI for resolving images
+        String baseUri = FileSystems.getDefault()
+                .getPath("src", "main", "resources", "static")
+                .toUri()
+                .toString();
 
-            builder.withHtmlContent(html, baseUri);
-            builder.toStream(os);
+        builder.withHtmlContent(html, baseUri);
+        builder.toStream(pdfOutputStream);
+        builder.run();
 
-            /* Generate PDF */
-            builder.run();
+        // Step 2: Add page numbers using PDFBox
+        try (PDDocument document = PDDocument.load(pdfOutputStream.toByteArray())) {
+            int totalPages = document.getNumberOfPages();
+
+            for (int i = 0; i < totalPages; i++) {
+                PDPage page = document.getPage(i);
+                PDRectangle mediaBox = page.getMediaBox();
+                float y = 70; // 70 PX margin from bottom to give space for footer
+                float x = mediaBox.getWidth() - 100; // Setting alignment to the right with media query
+
+                try (PDPageContentStream contentStream = new PDPageContentStream(document, page, PDPageContentStream.AppendMode.APPEND, true)) {
+                    contentStream.beginText();
+                    contentStream.setFont(PDType1Font.HELVETICA, 10);
+                    contentStream.newLineAtOffset(x, y);
+                    contentStream.showText("Page " + (i + 1) + " of " + totalPages);
+                    contentStream.endText();
+                }
+            }
+
+            // Step 3: Save final PDF with page numbers
+            try (OutputStream os = new FileOutputStream("statement.pdf")) {
+                document.save(os);
+            }
         }
     }
 }
